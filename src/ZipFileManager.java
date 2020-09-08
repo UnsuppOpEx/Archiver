f8421d1e532d09651030725d1465f69f6b928236
Anton <zantn22@gmail.com>
2020-09-08 23:16:38 +0300

    Реализация метода createZip

--- src/ZipFileManager.java
+++ src/ZipFileManager.java
@@ -1,7 +1,12 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
-
+    private ZipEntry entry;
     private Path zipFile;
 
     public ZipFileManager(Path zipFile) {
@@ -9,12 +14,29 @@ public class ZipFileManager {
     }
 
     /**
-     * Create
+     * Создает архив если он не создан,
+     * считывает данные из файла и записывает в архив.
      * @param source
      * @throws Exception
      */
     public void createZip(Path source) throws Exception {
+        try (OutputStream out = Files.newOutputStream(zipFile);
+             InputStream input = Files.newInputStream(source)) {
 
+            ZipOutputStream zos = new ZipOutputStream(out);
+
+            entry = new ZipEntry(source.getFileName().toString());
+            zos.putNextEntry(entry);
+
+
+            for (int c = input.read(); c != -1; c = input.read()) {
+                zos.write(c);
+            }
+            zos.flush();
+            zos.close();
+//            zos.closeEntry();
+
+        }
     }
 
 }
