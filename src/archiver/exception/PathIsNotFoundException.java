29225a1cb4dc6e99f2ca889e4b93bf11f8865b31
Anton <zantn22@gmail.com>
2020-09-17 20:58:21 +0300

    Создали 2 исключения

---
+++ src/archiver/exception/PathIsNotFoundException.java
@@ -0,0 +1,7 @@
+package archiver.exception;
+
+/**
+ * Кидает исключение когда не может найти путь
+ */
+public class PathIsNotFoundException extends Exception {
+}
---
+++ src/archiver/exception/WrongZipFileException.java
@@ -0,0 +1,7 @@
+package archiver.exception;
+
+/**
+ * Кидает исключение когда не может произвести каких-либо действий с архивом
+ */
+public class WrongZipFileException extends Exception {
+}
