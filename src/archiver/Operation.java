9a61ac78622ce9858f73626ae2e9d8bb94942530
Anton <zantn22@gmail.com>
2020-09-26 12:19:58 +0300

    Рефакторинг ENUM Operation.java

--- src/archiver/Operation.java
+++ src/archiver/Operation.java
@@ -5,11 +5,22 @@ package archiver;
  */
 public enum Operation {
 
-    CREATE,
-    ADD,
-    REMOVE,
-    EXTRACT,
-    CONTENT,
-    EXIT
+    CREATE("упаковать файлы в архив"),
+    ADD("добавить файл в архив"),
+    REMOVE("удалить файл из архива"),
+    EXTRACT("распаковать архив"),
+    CONTENT("просмотреть содержимое архива"),
+    EXIT("выход");
+
+    private String line;
+
+    Operation(String s) {
+        this.line = s;
+    }
+
+    @Override
+    public String toString() {
+        return line;
+    }
 
 }
