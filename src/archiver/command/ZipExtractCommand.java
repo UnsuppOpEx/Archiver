dc7b045c5e8c3a947eafa51ec6e466bcfc519ce9
Anton <zantn22@gmail.com>
2020-09-29 19:58:15 +0300

    Реализация метода extractAll для просмотра содержимого файлов архива и их свойств.

--- src/archiver/command/ZipExtractCommand.java
+++ src/archiver/command/ZipExtractCommand.java
@@ -1,11 +1,31 @@
 package archiver.command;
 
+import archiver.ConsoleHelper;
+import archiver.ZipFileManager;
+import archiver.exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Команда распаковки архива
  */
 public class ZipExtractCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Распаковка архива.");
 
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Введите полное имя файла или директории для распаковки:");
+            Path sourcePath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.extractAll(sourcePath);
+
+            ConsoleHelper.writeMessage("Архив распакован.");
+
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+        }
     }
 }
